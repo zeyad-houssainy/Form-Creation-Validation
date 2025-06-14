63f784a3b0f1180cab9b228e073cd5ed4bbd5ecf
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2025-06-15 01:05:10 +0300

    0. Form Creation and Validation

--- script.js
+++ script.js
@@ -5,41 +5,46 @@ document.addEventListener("DOMContentLoaded", () => {
     form.addEventListener("submit", () => {
         event.preventDefault()
 
-        // Retrieve User Inputs by its respective ID: username, email, and password.
-        const usernameInput = getElementById("username").value.trim()
-        const emailInput = getElementById("email").value.trim()
-        const passwordInput = getElementById("password").value.trim()
 
-        let isValid = true
-        let messages = []
-
-        // Username Validation:
-        if (usernameInput.length < 3){
-            isValid = false
-            messages.push("Your Username lenght must be more than 3 characters")
+        function validateForm() {
+            // Retrieve User Inputs by its respective ID: username, email, and password.
+            const usernameInput = getElementById("username").value.trim()
+            const emailInput = getElementById("email").value.trim()
+            const passwordInput = getElementById("password").value.trim()
+    
+            let isValid = true
+            let messages = []
+    
+            // Username Validation:
+            if (usernameInput.length < 3){
+                isValid = false
+                messages.push("Your Username lenght must be more than 3 characters")
+            }
+    
+            // Email Validation: must have character "@" and "."
+            if (!(emailInput.includes("@") && emailInput.includes("."))) {
+                isValid = false
+                messages.push("Enter a valid Email..") 
+            }
+            
+            // Password Validation: must be more than 8 characters
+            if (passwordInput.length < 8) {
+                isValid = false
+                messages.push("Password should be longer than 8 characters") 
+            }
+    
+            // Feedback Display Logic
+            feedbackDiv.style.display = "block";
+            
+            if(isValid) {
+                feedbackDiv.textContent = "Registration successful!"
+                feedbackDiv.style.color = "#28a745"
+            } else {
+                feedbackDiv.innerHTML = messages.join("<br>")
+                feedbackDiv.style.color = "#dc3545"
+            }
         }
 
-        // Email Validation: must have character "@" and "."
-        if (!(emailInput.includes("@") && emailInput.includes("."))) {
-            isValid = false
-            messages.push("Enter a valid Email..") 
-        }
-        
-        // Password Validation: must be more than 8 characters
-        if (passwordInput.length < 8) {
-            isValid = false
-            messages.push("Password should be longer than 8 characters") 
-        }
-
-        // Feedback Display Logic
-        feedbackDiv.style.display = "block";
-        
-        if(isValid) {
-            feedbackDiv.textContent = "Registration successful!"
-            feedbackDiv.style.color = "#28a745"
-        } else {
-            feedbackDiv.innerHTML = messages.join("<br>")
-            feedbackDiv.style.color = "#dc3545"
-        }
+        validateForm()
     })
 })
